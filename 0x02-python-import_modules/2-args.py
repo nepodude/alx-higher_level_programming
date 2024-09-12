b85fa5683fe21ab1a08cbf8855db055d8bc3f6bb
nepodude <nepo.jean73@gmail.com>
2024-09-13 00:13:32 +0200

    ok

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,9 @@ number = len(argv)
 if number == 1:
     print("0 arguments.")
 else:
-    print("{} arguments:".format(number - 1))
+    if number != 2:
+        print("{} arguments:".format(number - 1))
+    elif number == 2:
+        print("{} argument:".format(number - 1))
     for i in range(1, number):
         print("{}: {}".format(i, argv[i]))
