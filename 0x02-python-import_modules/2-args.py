991c879df17bf161540ec92d311db606fdec3359
nepodude <nepo.jean73@gmail.com>
2024-09-13 00:10:24 +0200

    ok

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-argv  = sys.argv
+argv = sys.argv
 number = len(argv)
 if number == 1:
     print("0 arguments.")
@@ -9,4 +9,3 @@ else:
     print("{} arguments:".format(number - 1))
     for i in range(1, number):
         print("{}: {}".format(i, argv[i]))
-    
