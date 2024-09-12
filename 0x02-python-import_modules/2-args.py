8c2583ab2c7cc1672787d08170f605109755bec7
nepodude <nepo.jean73@gmail.com>
2024-09-13 00:06:13 +0200

    ok

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+argv  = sys.argv
+number = len(argv)
+if number == 1:
+    print("0 arguments.")
+else:
+    print("{} arguments:".format(number - 1))
+    for i in range(1, number):
+        print("{}: {}".format(i, argv[i]))
+    
