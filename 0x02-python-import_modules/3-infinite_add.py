e39d73486da0a455564cb30d6be85e0f4b2fc689
nepodude <nepo.jean73@gmail.com>
2024-09-13 00:25:51 +0200

    ok

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+argv = sys.argv[1:]
+args = len(argv)
+sum = 0
+if args == 0:
+    print(0)
+elif args > 0:
+    for i in range(args):
+        sum += argv[i]
+    print(sum)
