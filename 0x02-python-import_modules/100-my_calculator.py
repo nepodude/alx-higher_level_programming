ff55b1476e947c0acd6513d377335a5ce679c2f5
nepodude <nepo.jean73@gmail.com>
2024-09-13 01:10:35 +0200

    okay

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mu, div
+    from calculator_1 import add, sub, mul, div
     import sys
 if len(sys.argv) - 1 != 3:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
