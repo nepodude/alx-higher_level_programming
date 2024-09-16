3adcf76bf2609c19314b26dde616c1a957a0a8f4
nepodude <nepo.jean73@gmail.com>
2024-09-16 15:20:30 +0200

    I got it

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list = []:
+        return None
+    else:
+        duplicate = [item for item in my_list]
+        total = 0
+        length = len(duplicate)
+        for i in length:
+            for item in duplicate:
+                total += item
+            average = total / len(duplicate)
+            duplicate = [x for x in duplicate if x >= average]
+    return duplicate[0]
