eae6d4ff52b4a7134b1f043c228094e2a0c33078
nepodude <nepo.jean73@gmail.com>
2024-09-18 17:03:47 +0200

    okay I got it

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    keeper = 0
+    helper = 0
+    for i in a_dictionary.values():
+        keeper = i
+        if keeper > helper:
+            helper = keeper
+    return
+    (key for key in a_dictionary.keys() if a_dictionary[key] == helper)
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    del simple_delete[key]
+    
