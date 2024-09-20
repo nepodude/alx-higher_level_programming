d71174d551b96772558d4711f71b290746679d7c
nepodude <nepo.jean73@gmail.com>
2024-09-20 16:11:17 +0200

    okay

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     keeper = 0
-    helper = 0
     for i in a_dictionary.values():
-        keeper = i
-        if keeper > helper:
-            helper = keeper
+        if i > keeper:
+            keeper = i
     return
-    (key for key in a_dictionary.keys() if a_dictionary[key] == helper)
+    (key for key in a_dictionary.keys() if a_dictionary[key] == keeper)
