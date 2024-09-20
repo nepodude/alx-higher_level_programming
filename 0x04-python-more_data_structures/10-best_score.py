dc1026e8e4fcec0c60eb51bdfa5149e4b5342911
nepodude <nepo.jean73@gmail.com>
2024-09-20 16:23:17 +0200

    got it

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-if not a_dictionary:
-return None
+	if not a_dictionary:
+		return None
 
-keeper = max(a_dictionary.values())
-
-for key, value in a_dictionary.items():
-if value == keeper:
+	keeper = max(a_dictionary.values())
+	
+	for key, value in a_dictionary.items():
+		if value == keeper:
