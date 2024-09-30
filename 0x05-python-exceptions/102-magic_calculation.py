4e3a4fb5912ce87e66a80ba3104d960258c643a0
nepodude <nepo.jean73@gmail.com>
2024-10-01 00:17:26 +0200

    easy

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 4):  # Loop from 1 to 3 (inclusive)
+        try:
+            if i > a:
+                raise Exception("Too far")
+            result += (a ** b) / i
+        except Exception as e:
+            result = a + b  # if an exception occurs, sum a and b
+            break  # exit the loop
+    return result
