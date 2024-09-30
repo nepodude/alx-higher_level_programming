dd93a96cea743ed288d1de9d7d47ce04e674cd86
nepodude <nepo.jean73@gmail.com>
2024-09-30 22:35:45 +0200

    got it

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    for j in my_list:
+        if i < x:
+            try:
+                print("{:d}".format(j), end='')
+                i += 1
+            except Exception as e:
+                continue
+    print()
+    return i
