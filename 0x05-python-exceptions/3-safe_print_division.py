85e98f056b0bdae686a834f868ccb9a32b70270f
nepodude <nepo.jean73@gmail.com>
2024-09-30 23:21:42 +0200

    got this

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        answer = a / b
+    except Exception as e:
+        answer = None
+    finally:
+        print("Inside result: {}".format(answer))
+        return answer
