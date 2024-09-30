376dd6b2f2e908657779bdd81405c6e247f2f560
nepodude <nepo.jean73@gmail.com>
2024-10-01 00:15:59 +0200

    easy

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        sys = __import__('sys')
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
