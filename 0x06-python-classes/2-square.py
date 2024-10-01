dc9e6e12be95d37669cb54918a0192107604fe52
nepodude <nepo.jean73@gmail.com>
2024-10-01 18:33:57 +0200

    corrected anerror

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square(object):
 
     """Represents a square."""
     def __init__(self, size=0):
-        if not isinstance(size):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             ValueError("size must be >= 0")
