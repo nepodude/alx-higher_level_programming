51f4b485b31ea2e4646d8fc1c9bf366083f10179
nepodude <nepo.jean73@gmail.com>
2024-10-01 18:36:35 +0200

    got it

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+"""Defines an empty class square"""
+
+
+class Square(object):
+
+    """Represents a square."""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        return size**2
