2d663d3c609fac063a1c05720dd02b5426f6eda8
nepodude <nepo.jean73@gmail.com>
2024-10-25 11:08:37 +0200

    coded it

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""initiates empty class"""
+
+
+class BaseGeometry:
+    """Define a puclic method called area"""
+    def area(self):
+        """Raise an exception error"""
+        raise Exception("area() is not implemented")
