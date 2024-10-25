df698aa073b54a78395c38c7b01faddaa812ce8a
nepodude <nepo.jean73@gmail.com>
2024-10-25 11:26:53 +0200

    did task number 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""initiates empty class"""
+
+
+class BaseGeometry:
+    """Define a puclic method called area"""
+    def area(self):
+        """Raise an exception error"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """Validates for the integer"""
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
