ec96df5fb69e592f94fdcb4a645c5133f56fb48a
nepodude <nepo.jean73@gmail.com>
2024-10-25 18:08:27 +0200

    fixed a bug

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,5 +8,6 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """initialize and validate size"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
