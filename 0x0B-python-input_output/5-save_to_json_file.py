5065f1ca453f1800bf31559e793acc47addd7a7a
nepodude <nepo.jean73@gmail.com>
2024-10-26 10:57:12 +0200

    done

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""initialize a function"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """write an object into a filename"""
+    with open(filename, "w", encoding='utf-8') as file:
+        json.dumps(my_obj, file)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""initialize a function"""
+import json
+
+
+def def load_from_json_file(filename):
+    """write an object into a filename"""
+    with open(filename, "w", encoding='utf-8') as file:
+        json.loads(file)
