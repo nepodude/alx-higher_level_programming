e18b0f4a9a7cdaab35db66817de033078673b64e
nepodude <nepo.jean73@gmail.com>
2024-10-26 10:43:54 +0200

    done

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""initialize a function"""
+
+
+def to_json_string(my_obj):
+    """return json string"""
+    return json.dumps(my_obj)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""initialize a function"""
+
+
+def from_json_string(my_str):
+    """return python data structure."""
+    return json.loads(my_str)
