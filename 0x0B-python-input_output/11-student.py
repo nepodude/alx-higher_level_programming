264374a158583c6e245377e7bb49cbe5a3db87e3
nepodude <nepo.jean73@gmail.com>
2024-10-26 16:17:30 +0200

    done

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Define a student"""
+
+
+class Student:
+    """initialize the class"""
+
+    def __init__(self, first_name, last_name, age):
+        """define the attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return stuffs"""
+        if (type(attrs) is list and all(type(ele) is str for ele in attrs)):
+            return ({k: getattr(self, k) for k in attrs if hasattr(self, k)})
+        else:
+            return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """going to replace all attributes of the Student instance"""
+        self.__dict__ = json
