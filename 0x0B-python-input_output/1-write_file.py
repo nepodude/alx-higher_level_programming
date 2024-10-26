cf4d290dbf7d2accd01fd4a12d0f3f17aea1229e
nepodude <nepo.jean73@gmail.com>
2024-10-26 10:29:39 +0200

    done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""initiating the function"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns
+    the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
