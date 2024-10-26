ff29172225673995a00a0b85960f81e8165f25ee
nepodude <nepo.jean73@gmail.com>
2024-10-26 15:55:32 +0200

    done it

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,6 +5,7 @@
 class Student:
     """initialize the class"""
     def __init__(self, first_name, last_name, age):
+        """define the attributes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -12,6 +13,7 @@ class Student:
     """retrives a dictioanary representation of a Student
     instance."""
     def to_json(self, attrs=None):
+        """return stuffs"""
         if type(attrs) is list and type(element) is str for element in attrs:
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dir__
