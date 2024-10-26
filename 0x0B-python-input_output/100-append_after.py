3ad950b96d4a0cf4297d48701d110ed31ef214d4
nepodude <nepo.jean73@gmail.com>
2024-10-26 17:24:21 +0200

    done

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""A function to perform cool stuffs"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """open a file and append stuffs in it"""
+    with open(filename, "w", encoding="utf-8") as file:
+        for line in file:
+            if search_string in line:
+                line += search_string
