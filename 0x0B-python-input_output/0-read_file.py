77faa7e0b328f489dab9336600597ebc546406a1
nepodude <nepo.jean73@gmail.com>
2024-10-26 09:42:10 +0200

    done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A function to read and print file"""
+
+
+def read_file(filename=""):
+    """initialize a function"""
+    with open(filename, 'r') as my_file:
+        print(my_file.read())
