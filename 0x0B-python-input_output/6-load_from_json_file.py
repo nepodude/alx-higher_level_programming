068bf262ac4b04a2e8e203922e2c9fd35ecad258
nepodude <nepo.jean73@gmail.com>
2024-10-26 11:10:17 +0200

    got it

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def def load_from_json_file(filename):
     """write an object into a filename"""
-    with open(filename, "w", encoding='utf-8') as file:
-        json.load(file)
+    with open(filename, encoding='utf-8') as file:
+        return json.load(file)
