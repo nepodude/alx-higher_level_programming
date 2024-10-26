3e5b778f617d1ae1a8f8d15818eda4f51d059460
nepodude <nepo.jean73@gmail.com>
2024-10-26 17:06:59 +0200

    done

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""pascal triangle is great"""
+
+
+def fact(n):
+    """pascal triangle is great"""
+    if n == 0:
+        return 1
+    keeper = 1
+    for i in range(1, n):
+        keeper *= i
+    keeper *= n
+    return keeper
+
+
+def combinatorial(i, j):
+    """pascal triangle is great"""
+    return int(fact(j) / (fact(j - i) * fact(i)))
+
+
+def pascal_triangle(n):
+    """pascal triangle is great"""
+    if n <= 0:
+        return []
+    i = 0
+    mylist = []
+    for i in range(n):
+        helper = []
+        for j in range(i + 1):
+            helper.append(combinatorial(j, i))
+        mylist.append(helper)
+    return mylist
