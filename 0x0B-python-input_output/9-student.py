0149c23056f08e63ff4c92491c725506ec31f3cf
nepodude <nepo.jean73@gmail.com>
2024-10-26 15:32:20 +0200

    done it

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Define a student"""
+
+
+helper = __import__('8-class_to_json').class_to_json
+class Student:
+    """initialize the class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """retrives a dictioanary representation of a Student
+    instance."""
+    def to_json(self):
+        return helper(self)
