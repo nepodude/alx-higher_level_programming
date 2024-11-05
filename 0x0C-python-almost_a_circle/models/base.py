98b80d719768eed58fbe7e227a4368f69ce2adda
nepodude <nepo.jean73@gmail.com>
2024-11-05 22:31:22 +0200

    debugging

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,13 @@
-# models/base.py
+#!/usr/bin/python3
+
+"""defines Base class for future use."""
+
 
 class Base:
     """
     Base class for managing `id` attribute in future classes.
     This helps centralize `id` management, avoiding repetitive code.
     """
-    # Private class attribute to count objects created from Base class
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -17,9 +19,7 @@ class Base:
                   If not provided, an automatic id is assigned.
         """
         if id is not None:
-            # Assign the given id if provided
             self.id = id
         else:
-            # Increment the count and assign it to the instance id
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
