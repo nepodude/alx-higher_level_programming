23372eff6b6510b2d5cc7e7b36d0c9ddf6294e2a
nepodude <nepo.jean73@gmail.com>
2024-11-06 01:01:09 +0200

    new task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -78,6 +78,6 @@ class Square(Rectangle):
         }
 
     return (
-            f"[Square] ({self.id}) {self.x}/{self.y} - "
-            f"{self.width}"
+        f"[Square] ({self.id}) {self.x}/{self.y} - "
+        f"{self.width}"
         )
