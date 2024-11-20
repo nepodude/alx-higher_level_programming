1fbd7e3687ca144bbbcd5098d03109c54c6c001a
nepodude <nepo.jean73@gmail.com>
2024-11-20 15:13:39 +0200

    debugged

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creates a table with an id an a value.
-CREATE TABLE IF NOT EXISTS `fist_table` (`id` = INT, `name` = VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
