0e48106485d8791dd288c48743d8afcbae955b8d
nepodude <nepo.jean73@gmail.com>
2024-12-02 12:57:50 +0200

    coded it.

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- create the database hbtn_0d_2 if it doesn't already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- create the user user_0d_2 with specified password.
+CREATE DATABASE IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+--grant SELECT privilege on the database hbtn_0d_2 to the user user_0d_2.
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- flush privileges to take effect immediately.
+FLUSH PRIVILEGES;
