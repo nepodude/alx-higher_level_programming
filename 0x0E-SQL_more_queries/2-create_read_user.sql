0e22fe24a5f6197cc1b87566b44e61550ba1534b
nepodude <nepo.jean73@gmail.com>
2024-12-02 13:04:28 +0200

    coded it.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,5 +7,3 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 --grant SELECT privilege on the database hbtn_0d_2 to the user user_0d_2.
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
--- flush privileges to take effect immediately.
-FLUSH PRIVILEGES;
