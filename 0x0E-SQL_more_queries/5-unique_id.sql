e64a08bf0099d9e3d3d59d927f0f41087ddcbc7e
nepodude <nepo.jean73@gmail.com>
2024-12-02 13:29:24 +0200

    Debugged it

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- create a table unique_id and won't fail if it does exits.
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAUL 1 UNIQUE,
+    id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
