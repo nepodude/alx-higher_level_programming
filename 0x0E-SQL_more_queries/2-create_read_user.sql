07a2039c43338b411193c31c66a21cc4fed1a8a6
nepodude <nepo.jean73@gmail.com>
2024-12-02 13:05:31 +0200

    Debugged it.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create the user user_0d_2 with specified password.
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 
 --grant SELECT privilege on the database hbtn_0d_2 to the user user_0d_2.
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 
