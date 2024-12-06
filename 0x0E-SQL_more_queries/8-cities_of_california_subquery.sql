8cd4f546972fb099fe0939a6d54d85a353a329e9
nepodude <nepo.jean73@gmail.com>
2024-12-06 10:03:14 +0200

    updated task 8, and 9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,11 @@
 -- Solve the task at hand.
 USE hbtn_0d_usa;
 
-SELECT cities.name
-FROM cities, states
-WHERE cities.state_id = states.id
-  AND states.name = 'California'
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT states.id
+    FROM states
+    WHERE states.name = 'California'
+)
 ORDER BY cities.id ASC;
