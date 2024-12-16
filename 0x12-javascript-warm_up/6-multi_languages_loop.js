f4fe12a1928c68126a1eb0b4915aa30cc8f8451d
nepodude <nepo.jean73@gmail.com>
2024-12-16 18:37:04 +0200

    corrected semistandard bugs

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myarray = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const myarray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let helper = 0;
 while(helper < myarray.length) {
     console.log(myarray[helper]);
