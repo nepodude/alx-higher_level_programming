bfd85574159f3841591f7956a6f0c8749d4fc253
nepodude <nepo.jean73@gmail.com>
2025-01-09 14:36:29 +0200

    debugged task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-int1 = parseInt(process.argv[2]);
-int2 = parseInt(process.argv[3]);
+let int1 = parseInt(process.argv[2]);
+let int2 = parseInt(process.argv[3]);
 console.log(int1 + int2)
