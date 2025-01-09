cb3b8720f6af0f7d4a02a4b1061e743b39cf301d
nepodude <nepo.jean73@gmail.com>
2025-01-09 14:32:05 +0200

    debugged task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-    console.log('Missing number of occurrences');
+  console.log('Missing size');
 }
 
 let helper = parseInt(process.argv[2]);
 let helper1 = helper;
 while (helper > 0) {
-    console.log("X".repeat(helper1));
-    helper --;
+  console.log("X".repeat(helper1));
+  helper --;
 }
