de17419ec9c87126804970e1a993048a7c75387f
nepodude <nepo.jean73@gmail.com>
2025-01-09 14:44:24 +0200

    debugged task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -6,7 +6,7 @@ function factorial (n) {
   if (n === 0 || isNaN(n)) {
     return (1);
   }
-    return (n * factorial(n - 1));
+  return (n * factorial(n - 1));
 }
 
 console.log(factorial(parseInt(process.argv[2])));
