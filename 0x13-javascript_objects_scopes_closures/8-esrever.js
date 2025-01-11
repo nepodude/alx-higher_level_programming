bad3089e25abf8741b1a9f7b19f258bf8eeac217
nepodude <nepo.jean73@gmail.com>
2025-01-11 22:52:18 +0200

    updated task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const reversedList = [];
-  for (let i = list.length - 1; i > 0; i--) {
+  for (let i = list.length - 1; i > -1; i--) {
     reversedList.push(list[i]);
   }
   return reversedList;
-}
+};
