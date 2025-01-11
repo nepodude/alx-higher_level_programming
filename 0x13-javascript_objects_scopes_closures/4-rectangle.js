c2bfb2a4be00818ee1c069b0d5120de37d4afa55
nepodude <nepo.jean73@gmail.com>
2025-01-11 22:03:06 +0200

    updated task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     if (this.width && this.height) {
       for (let i = 0; i < this.height; i++) {
         console.log('X'.repeat(this.width));
@@ -15,16 +15,16 @@ class Rectangle {
     }
   }
 
-  rotate() {
+  rotate () {
     const helper = this.height;
     this.height = this.width;
     this.width = helper;
   }
 
-  double() {
+  double () {
     this.height *= 2;
     this.width *= 2;
-  }  
+  }
 }
 
 module.exports = Rectangle;
