d1398137f4cff076703f5928c047540dfb98a5a3
nepodude <nepo.jean73@gmail.com>
2025-01-12 11:43:59 +0200

    updated task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const [userId, occurrence] of Object.entries(dict)) {
+  if (!newDict[occurrence]) {
+    newDict[occurrence] = [];
+  }
+  newDict[occurrence].push(userId);
+}
+console.long(dict);
