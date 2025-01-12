71a81515bfa21ec5bcabf14135879650b7199d8a
nepodude <nepo.jean73@gmail.com>
2025-01-12 11:27:54 +0200

    updated task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list1 = require('./100-data');
+const list1 = require('./100-data').list;
 console.log(list1);
 const list2 = list2.map((value, index) => value * index);
 console.log(list2);
