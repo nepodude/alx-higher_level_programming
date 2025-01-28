4d30471a31cbc1804db2068ff5b786d6c31ac011
nepodude <nepo.jean73@gmail.com>
2025-01-28 17:28:28 +0200

    updated

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""  lists all states where name matches the argument. from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    argument = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states\
+        WHERE name LIKE BINARY %s ORDER BY id ASC;", (argument,)
+    )
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
