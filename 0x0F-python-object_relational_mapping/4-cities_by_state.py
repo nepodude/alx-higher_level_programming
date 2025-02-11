285ded299bb150d96290bd0a8c3c715c94c8d884
nepodude <nepo.jean73@gmail.com>
2025-02-11 15:30:18 +0200

    updated

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  lists all cities from the database hbtn_0e_0_usa """
+"""Write a script that lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
@@ -17,8 +17,11 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM cities ORDER BY id ASC;")
+    query = """SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC;"""
+    cursor.execute(query)
 
     cities = cursor.fetchall()
 
